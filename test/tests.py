d6977a2363b4453d3fb5586487ecc031bc2e7a53
Sandro Böhme <sandro.boehme@gmx.de>
2019-04-13 20:54:44 +0200

    added test for posting documents with a fields structure

--- test/tests.py
+++ test/tests.py
@@ -6,15 +6,14 @@ from firestorehttp.jwt import JWT
 
 
 class TestFirestoreAccess(unittest.TestCase):
+    base_api_url = 'https://content-firestore.googleapis.com/v1'
+    project = 'example5-237118'
+    database = '/databases/(default)/'
 
     def test_create_jwt(self):
-
         jwt = JWT()
         token = 'Bearer ' + jwt.get_token().decode("utf-8")
-        base_api_url = 'https://content-firestore.googleapis.com/v1'
-        project = 'example5-237118'
-        database = '/databases/(default)/'
-        endpoint_prefix = base_api_url + '/projects/' + project + database
+        endpoint_prefix = self.base_api_url + '/projects/' + self.project + self.database
 
         # list collections
         endpoint = 'documents:listCollectionIds'
@@ -24,12 +23,54 @@ class TestFirestoreAccess(unittest.TestCase):
         endpoint = 'documents/collection1'
         print(requests.get(endpoint_prefix + endpoint, headers={'Authorization': token}).text)
 
+    def test_post_document(self):
+        jwt = JWT()
+        token = 'Bearer ' + jwt.get_token().decode("utf-8")
+        endpoint_prefix = self.base_api_url + '/projects/' + self.project + self.database
+
         # add document to collection
-        endpoint = 'documents/collection1/'
+        endpoint = 'documents/collection3'
         params = {
-            'documentId': 'documentId2'
+            'documentId': 'd6'
         }
-        print(requests.post(endpoint_prefix + endpoint, headers={'Authorization': token}, params=params).text)
+        field = {
+            "fields":
+                {
+                    "f1":
+                        {
+                            "mapValue":
+                                {
+                                    "fields":
+                                        {
+                                            "mf1":
+                                                {
+                                                    "booleanValue": True,
+                                                },
+                                            "mf2":
+                                                {
+                                                    "stringValue": "xyz",
+                                                },
+                                            "mf3":
+                                                {
+                                                    "integerValue": 42,
+                                                }
+                                        }
+                                }
+                        },
+                    "f2":
+                        {
+                            "stringValue": "abcde",
+                        }
+                }
+        }
+
+        print(endpoint_prefix + endpoint)
+        print(requests.post(endpoint_prefix + endpoint,
+                            headers={'Authorization': token},
+                            params=params,
+                            json=field
+                            ).text
+              )
 
 
 if __name__ == '__main__':
